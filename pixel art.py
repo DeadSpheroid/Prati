1961ac662c41da0455937a46c45e4cd5dce9c529
WarrenJacinto <warrenwohlig@gmail.com>
2022-12-05 21:49:26 +0530

    added color compatibility/too slow to be useful

--- pixel art.py
+++ pixel art.py
@@ -4,38 +4,36 @@ import turtle
 
 # Binary Image
 
-img = cv2.imread('./image.jpg',0)
-ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+img = cv2.imread('./praticolortext.jpg', cv2.IMREAD_COLOR)
+
 width = int(img.shape[1])
 height = int(img.shape[0])
-print(bw_img)
-print(img)
 
 # Turtle Setup
 
 my_screen = turtle.Screen()
 my_screen.screensize(width, height)
+my_screen.colormode(255)
 my_screen.bgcolor('black')
 my_pen = turtle.Turtle()
 my_pen.color('white')
-my_screen.tracer(16)
-#my_pen.hideturtle()
-my_pen.pencolor("white")
+my_screen.tracer(128)
 # Printing Loop
+
+pixR, pixG, pixB = 0, 0, 0
 
 for i in range(int(height/2), int(height/-2),  -1):
     my_pen.penup()
     my_pen.goto(-(width / 2), i)
+    my_pen.pendown()
 
     for l in range(-int(width/2), int(width/2), 1):
         pix_width = int(l + (width/2))
         pix_height = int(height/2 - i)
-        if bw_img[pix_height, pix_width] == 0:
-            my_pen.pendown()
-            my_pen.forward(1)
-        else:
-            my_pen.penup()
-            my_pen.forward(1)
-    my_screen.update()
+        pixB = img[pix_height][pix_width][0]
+        pixG = img[pix_height][pix_width][1]
+        pixR = img[pix_height][pix_width][2]
+        my_pen.pencolor(pixR, pixG, pixB)
+        my_pen.forward(1)
 
 turtle.done()
