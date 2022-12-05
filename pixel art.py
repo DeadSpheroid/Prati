47f3fb24e6dac8b241d9c9840f27267d94361be3
WarrenJacinto <warrenwohlig@gmail.com>
2022-12-05 23:00:26 +0530

    new image with text/optimised for speed/removed debug code

--- pixel art.py
+++ pixel art.py
@@ -1,26 +1,22 @@
 import cv2
 import turtle
 
-
 # Binary Image
 
-img = cv2.imread('./image.jpg',0)
+img = cv2.imread('./imagetext.jpg', 0)
 ret, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 width = int(img.shape[1])
 height = int(img.shape[0])
-print(bw_img)
-print(img)
 
 # Turtle Setup
 
 my_screen = turtle.Screen()
 my_screen.screensize(width, height)
 my_screen.bgcolor('black')
+my_screen.tracer(0)
 my_pen = turtle.Turtle()
 my_pen.color('white')
-my_screen.tracer(16)
-#my_pen.hideturtle()
-my_pen.pencolor("white")
+my_pen.pencolor('white')
 # Printing Loop
 
 for i in range(int(height/2), int(height/-2),  -1):
